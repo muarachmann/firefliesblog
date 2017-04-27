d95a47bbd822aa226eb19d7866f40d97ba78315c
pacicap <abandapacicap@gmail.com>
2017-04-27 05:19:12 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -5,7 +5,10 @@ int main(){
 
 printf("Hello world");
 printf("Atah u are not normal");
+  Printf ("hummmm") ;
 prinf("Hey paci i added this.(^_^)");
+  Printf("Yeah I saw it") ;
   printf("Where are you na paci");
+  Printf ("I am there ohh") ;
 return 0;
 }
